3886c3072e8760032d5d77935e72d72a3526ad6d
ssherliann <137110094+ssherliann@users.noreply.github.com>
2024-01-07 18:10:09 +0200

    upd 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,30 +1,30 @@
 const form = document.querySelector('.feedback-form');
 
 const savedFormData = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
-form.email.value = savedFormData.email || '';
-form.message.value = savedFormData.message || '';
+form.email.value = (savedFormData.email || '').trim();
+form.message.value = (savedFormData.message || '').trim();
 
 form.addEventListener('input', function (event) {
     if (event.target.name === 'email' || event.target.name === 'message') {
-
-    const currentState = {
-        email: form.email.value,
-        message: form.message.value
-    };
-    localStorage.setItem('feedback-form-state', JSON.stringify(currentState));
+        
+        const currentState = {
+            email: form.email.value.trim(),
+            message: form.message.value.trim()
+        };
+        
+        localStorage.setItem('feedback-form-state', JSON.stringify(currentState));
     }
 });
 
-    form.addEventListener('submit', function (event) {
+form.addEventListener('submit', function (event) {
     event.preventDefault();
 
     const formData = {
-    email: form.email.value,
-    message: form.message.value
+        email: form.email.value.trim(),
+        message: form.message.value.trim()
     };
     console.log(formData);
 
     form.reset();
     localStorage.removeItem('feedback-form-state');
 });
-
